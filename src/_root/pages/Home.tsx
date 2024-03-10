d02447f1acd8387f9302b5809a067d37f610e5fd
Sirimalle Krushi <sirimallekrushi9999@gmail.com>
2024-03-10 17:27:59 +0530

    dialogue box added to create moment

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,16 +1,24 @@
 import CreateMoment from "@/components/shared/CreateMoment";
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { useSelector } from "react-redux";
 
 const Home: React.FC = () => {
   const user = useSelector((state: any) => state.user);
   const token = useSelector((state: any) => state.token);
-
   return (
-    <div>
-      <div>
+    <Dialog>
+      <DialogTrigger>
+        <Button>Create Moment</Button>
+      </DialogTrigger>
+      <DialogContent>
         <CreateMoment />
-      </div>
-    </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- src/_root/pages/NotFound.tsx
+++ src/_root/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl mb-4">404 - Page Not Found</h1>
+      <h1 className="font sans text-6xl mb-4">404 - Page Not Found</h1>
       <p className="mb-8">The page you are looking for does not exist.</p>
     </div>
   );
