dc56ebf7ed0e87575c9782f51f7c959847672699
SCharan1134 <sricharanrayala24@gmail.com>
2024-03-22 11:35:00 +0530

    added functionality to all and friends in home

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,22 +1,44 @@
 import MomentsWidget from "@/components/shared/MomentsWidget";
 import { useSelector } from "react-redux";
 import FriendSuggestionList from "@/components/shared/FriendSuggestionList";
+import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
   const user = useSelector((state: any) => state.user);
   const token = useSelector((state: any) => state.token);
+  const [isFriend, setIsFriend] = useState(false);
+  const [refreshKey, setRefreshKey] = useState(0);
+  useEffect(() => {
+    setRefreshKey((prevKey) => prevKey + 1); // Update refresh key
+  }, [isFriend]);
   return (
     <div className="flex w-full items-start">
       <div className="w-full pb-20 px-10 pt-10">
         <div className="flex w-full justify-between items-center pb-5">
           <div className="text-xl font-bold">Feed</div>
           <div className="flex justify-between items-center gap-5">
-            <button className="text-primary">Recent</button>
-            <button className="text-gray-400">Friends</button>
-            <button className="text-gray-400">Recommended</button>
+            <button
+              className={`${!isFriend ? "text-primary" : "text-gray-400"}`}
+              onClick={() => setIsFriend(false)}
+            >
+              all
+            </button>
+            <button
+              className={`${isFriend ? "text-primary" : "text-gray-400"}`}
+              onClick={() => {
+                setIsFriend(true);
+                console.log(isFriend);
+              }}
+            >
+              Friends
+            </button>
           </div>
         </div>
-        <MomentsWidget userId={user._id} />
+        <MomentsWidget
+          userId={user._id}
+          isFriends={isFriend}
+          refreshKey={refreshKey} // Pass refresh key to MomentsWidget
+        />
       </div>
       <div className="w-[650px] border-l border-black sticky top-0 h-screen px-2">
         <FriendSuggestionList />
--- src/components/shared/MomentsWidget.tsx
+++ src/components/shared/MomentsWidget.tsx
@@ -1,28 +1,50 @@
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setMoments } from "@/state";
 import Moment from "./Moment";
 
 interface MomentsWidgetProps {
   userId: string;
   isProfile?: boolean;
+  isAll?: boolean;
+  isFriends?: boolean;
+  refreshKey?: any;
 }
 
 const MomentsWidget: React.FC<MomentsWidgetProps> = ({
   userId,
   isProfile = false,
+  isFriends = false,
+  refreshKey,
 }) => {
   const dispatch = useDispatch();
   const token = useSelector((state: any) => state.token);
   const moments = useSelector((state: any) => state.moments);
+  const [key, setKey] = useState(0);
 
   const getMoments = async () => {
-    const response = await axios.get(`http://localhost:3001/moments/feed`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await axios.get(
+      `http://localhost:3001/moments/${userId}/feed`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+
+    dispatch(setMoments({ moments: response.data }));
+  };
+  const getFriendMoments = async () => {
+    const response = await axios.get(
+      `http://localhost:3001/moments/${userId}/feed/friend`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     console.log(response.data);
 
     dispatch(setMoments({ moments: response.data }));
@@ -43,23 +65,35 @@ const MomentsWidget: React.FC<MomentsWidgetProps> = ({
   useEffect(() => {
     if (isProfile) {
       getUserMoments();
+    } else if (isFriends) {
+      getFriendMoments();
     } else {
       getMoments();
     }
-  }, []);
+  }, [isProfile, isFriends, userId, token, refreshKey]);
+
+  useEffect(() => {
+    // When refreshKey changes, update the local key state to trigger a re-render
+    setKey((prevKey) => prevKey + 1);
+  }, [refreshKey]);
+
   return (
     <div className="flex flex-col items-center justify-center gap-10">
-      {moments.map((moment: any) => (
-        <Moment
-          postId={moment._id}
-          key={moment._id}
-          postUserId={moment.userId}
-          momentPath={moment.momentPath}
-          description={moment.description}
-          visibility={moment.visibility}
-          likes={moment.likes}
-        />
-      ))}
+      {moments.length > 0 ? (
+        moments.map((moment: any) => (
+          <Moment
+            postId={moment._id}
+            key={moment._id}
+            postUserId={moment.userId}
+            momentPath={moment.momentPath}
+            description={moment.description}
+            visibility={moment.visibility}
+            likes={moment.likes}
+          />
+        ))
+      ) : (
+        <p>No moments to display.</p>
+      )}
     </div>
   );
 };
