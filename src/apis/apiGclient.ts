ebe6e841789d1b7311de4ec21bcbbdab152a3ddd
SCharan1134 <sricharanrayala24@gmail.com>
2024-04-23 23:10:17 +0530

    changed api

--- src/apis/apiGclient.ts
+++ src/apis/apiGclient.ts
@@ -1,4 +1,5 @@
-export const api = "http://3.99.130.143:3001";
+// export const api = "http://3.99.130.143:3001";
+export const api = "https://moments-be.vercel.app/";
 // export const api = "http://localhost:3001";
 
 // export const apiLogin = `${api}/auth/login`;
